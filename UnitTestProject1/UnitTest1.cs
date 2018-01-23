6a505bcea828b6aa2457c833f6d081a080867f55
Tero Teelahti <tero@armadainteractive.com>
2018-01-23 10:41:11 +0200

    Convert to netstandard2.0. Convert unit tests to xunit.

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
 
 using ReliableNetcode;
 
 namespace UnitTestProject1
 {
-	[TestClass]
 	public class UnitTest1
 	{
 		// tests with randomness are run this many times to ensure tests are unlikely to randomly pass
@@ -15,7 +14,7 @@ namespace UnitTestProject1
 		/// <summary>
 		/// All messages are sent through the Reliable channel with no packet loss. All messages should be received.
 		/// </summary>
-		[TestMethod]
+		[Fact]
 		public void TestBasicSending()
 		{
 			List<byte> sentPackets = new List<byte>();
@@ -76,7 +75,7 @@ namespace UnitTestProject1
 		/// <summary>
 		/// Test ushort message ID wrapping
 		/// </summary>
-		[TestMethod]
+		[Fact]
 		public void TestIDWrapping()
 		{
 			List<byte> sentPackets = new List<byte>();
@@ -135,7 +134,7 @@ namespace UnitTestProject1
 		/// <summary>
 		/// Packets are sent through the Reliable channel with random packet loss. All packets should be received
 		/// </summary>
-		[TestMethod]
+		[Fact]
 		public void TestReliability()
 		{
 			Random rand = new Random();
@@ -210,7 +209,7 @@ namespace UnitTestProject1
 		/// <summary>
 		/// All packets are sent through the Unreliable channel with no packet loss. All packets should be received.
 		/// </summary>
-		[TestMethod]
+		[Fact]
 		public void TestBasicUnreliable()
 		{
 			List<byte> sentPackets = new List<byte>();
@@ -262,7 +261,7 @@ namespace UnitTestProject1
 		/// <summary>
 		/// All packets are sent through the UnreliableOrdered channel with no packet loss. All packets should be received.
 		/// </summary>
-		[TestMethod]
+		[Fact]
 		public void TestBasicUnreliableOrdered()
 		{
 			List<byte> sentPackets = new List<byte>();
@@ -314,7 +313,7 @@ namespace UnitTestProject1
 		/// <summary>
 		/// All packets are sent through the UnreliableOrdered channel with random reordering. Received packets should be in order.
 		/// </summary>
-		[TestMethod]
+		[Fact]
 		public void TestUnreliableOrderedSequence()
 		{
 			Random rand = new Random();
---
+++ publish.sh
@@ -0,0 +1,40 @@
+# CI Build script needed to run the whole build with one script on a docker container.
+# This build runs tests, and outputs a release NuGet package. If feed and feed key are 
+# defined then also publishes the package.
+#
+# Example to run on dev environment without docker, without publish
+#   ./publish.sh 0.1.0-something
+# 
+# Example inside docker with publish, emulating how it is run on Google Cloud Container Builder:
+#     docker run -v $(pwd):/workspace --entrypoint bash \
+#        microsoft/dotnet:2.0.5-sdk-2.1.4-stretch \
+#        /workspace/Assets/Plugins/GameModel/publish.sh 0.1.2-foo https://www.myget.org/F/games/api/v2/package [my-feed-key]
+
+
+# Exit on any error
+set -e
+
+VER=$1
+FEED=$2
+FEED_KEY=$3
+
+echo "Starting test, package and publish"
+
+if [ -d "/workspace" ]; then
+    echo "Switching to /workspace"
+    cd /workspace
+fi
+
+# Add other test projects to this list when needed
+dotnet test UnitTestProject1
+dotnet pack ReliableNetcode /p:PackageVersion=$VER --include-symbols --configuration Release
+
+if [ -z "$FEED" ]; then
+    echo "Package feed not defined, skipping package publish"
+else
+    echo "Publishing package $VER to $FEED"
+	dotnet nuget push -s $FEED -k $FEED_KEY ReliableNetcode/bin/Release/ReliableNetcode.$VER.nupkg
+	dotnet nuget push -s $FEED -k $FEED_KEY ReliableNetcode/bin/Release/ReliableNetcode.$VER.symbols.nupkg
+fi
+
+echo "OK"
